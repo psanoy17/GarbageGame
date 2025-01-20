524a1bf1344868e82b368c5fd83cbc779f4c12d6
psanoy17 <patrickaangelo@gmail.com>
2025-01-20 18:16:23 +0800

    added objective panel in levels

--- scenes/press_start.gd
+++ scenes/press_start.gd
@@ -1,31 +1,21 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_back_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-	pass # Replace with function body.
-
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
-	pass # Replace with function body.
-
 
 func _on_texture_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
 	pass # Replace with function body.
 
-
 func _on_texture_button_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level3.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
 	pass # Replace with function body.
